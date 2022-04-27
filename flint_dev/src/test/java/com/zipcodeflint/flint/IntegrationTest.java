50436fff8f3daa5458cebb714d0fc658230497d9
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-26 21:34:30 -0400

    Initial version of Flint generated by generator-jhipster@7.8.1

---
+++ flint_dev/src/test/java/com/zipcodeflint/flint/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.zipcodeflint.flint;
+
+import com.zipcodeflint.flint.FlintApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = FlintApp.class)
+public @interface IntegrationTest {
+}
