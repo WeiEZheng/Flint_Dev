47738a13349f6d146962c4dd07798ec9d7e06da1
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-04-30 16:40:14 -0400

    controller, services, etc for transaction

--- src/main/java/com/example/flint/controller/TransactionController.java
+++ src/main/java/com/example/flint/controller/TransactionController.java
@@ -1,4 +1,40 @@
 package com.example.flint.controller;
 
+import com.example.flint.model.Transaction;
+import com.example.flint.repository.TransactionRepository;
+import com.example.flint.service.TransactionServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/api")
 public class TransactionController {
+    private final TransactionServices transactionServices;
+
+    private final TransactionRepository transactionRepository;
+
+    public TransactionController(TransactionServices transactionServices, TransactionRepository transactionRepository) {
+        this.transactionServices = transactionServices;
+        this.transactionRepository = transactionRepository;
+    }
+
+    @PostMapping("/transactions")
+    public ResponseEntity<Transaction> createTransactions(@Validated @RequestBody Transaction transaction) throws URISyntaxException {
+        if (transaction.getId() != null) {
+//            need to throw exception for existing entity
+        }
+        Transaction result = transactionServices.save(transaction);
+        return ResponseEntity
+                .created(new URI("/api/transactions/" + result.getId()))
+                .header("message here")
+                .body(result);
+    }
 }
--- src/main/java/com/example/flint/service/TransactionServices.java
+++ src/main/java/com/example/flint/service/TransactionServices.java
@@ -1,4 +1,96 @@
 package com.example.flint.service;
 
+import com.example.flint.model.Transaction;
+import com.example.flint.model.enumeration.TransactionType;
+import com.example.flint.repository.TransactionRepository;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
 public class TransactionServices {
+    private final TransactionRepository transactionRepository;
+
+    public TransactionServices(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public Transaction save(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public Transaction update(Transaction transaction) {
+        return this.save(transaction);
+    }
+
+    public Optional<Transaction> partialUpdate(Transaction transaction) {
+        return transactionRepository
+                .findById(transaction.getId())
+                .map(existingTransactions -> {
+                    if (transaction.getDateOfTransaction() != null) {
+                        existingTransactions.setDateOfTransaction(transaction.getDateOfTransaction());
+                    }
+                    if (transaction.getTypeOfTransaction() != null) {
+                        existingTransactions.setTypeOfTransaction(transaction.getTypeOfTransaction());
+                    }
+                    if (transaction.getTransactionAmount() != null) {
+                        existingTransactions.setTransactionAmount(transaction.getTransactionAmount());
+                    }
+                    if (transaction.getToAccountNumber() != null) {
+                        existingTransactions.setToAccountNumber(transaction.getToAccountNumber());
+                    }
+                    if (transaction.getFromAccountNumber() != null) {
+                        existingTransactions.setFromAccountNumber(transaction.getFromAccountNumber());
+                    }
+
+                    return existingTransactions;
+                })
+                .map(this::save);
+    }
+
+    public List<Transaction> findAll() {
+        return transactionRepository.findAll();
+    }
+
+    public Optional<Transaction> findOne(Long id) {
+        return transactionRepository.findById(id);
+    }
+
+    public void delete(Long id) {
+        transactionRepository.deleteById(id);
+    }
+
+    public List<Transaction> findByDateOfTransaction(Instant dateOfTransaction) {
+        return transactionRepository.findByDateOfTransaction(dateOfTransaction);
+    }
+
+    public List<Transaction> findByDateOfTransactionIsBetween(Instant dateOfTransactionStart, Instant dateOfTransactionEnd) {
+        return transactionRepository.findByDateOfTransactionIsBetween(dateOfTransactionStart,dateOfTransactionEnd);
+    }
+
+    public List<Transaction> findByFromAccountNumber(Long fromAccountNumber) {
+        return transactionRepository.findByFromAccountNumber(fromAccountNumber);
+    }
+
+    public List<Transaction> findByToAccountNumber(Long toAccountNumber) {
+        return transactionRepository.findByToAccountNumber(toAccountNumber);
+    }
+
+    public List<Transaction> findByTypeOfTransaction(TransactionType typeOfTransaction) {
+        return transactionRepository.findByTypeOfTransaction(typeOfTransaction);
+    }
+
+    public List<Transaction> findByTransactionAmountIsBetween(BigDecimal transactionAmountStart,
+                                                              BigDecimal transactionAmountEnd) {
+        return transactionRepository.findByTransactionAmountIsBetween(transactionAmountStart,transactionAmountEnd);
+    }
+
+    public List<Transaction> findByTransactionAmountIsGreaterThanEqual(BigDecimal transactionAmount) {
+        return transactionRepository.findByTransactionAmountIsGreaterThanEqual(transactionAmount);
+    }
+
+    public List<Transaction> findByTransactionAmountLessThanEqual(BigDecimal transactionAmount) {
+        return transactionRepository.findByTransactionAmountLessThanEqual(transactionAmount);
+    }
 }
