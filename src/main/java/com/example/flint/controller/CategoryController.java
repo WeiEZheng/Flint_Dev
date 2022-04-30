739c7d2eba572735acf771ba755ce4cea3bf6667
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-30 19:36:37 -0400

    apis

--- src/main/java/com/example/flint/controller/CategoryController.java
+++ src/main/java/com/example/flint/controller/CategoryController.java
@@ -3,9 +3,12 @@ package com.example.flint.controller;
 import com.example.flint.model.Category;
 import com.example.flint.repository.CategoryRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -21,5 +24,11 @@ public class CategoryController {
     @GetMapping("/categories")
     Collection<Category> categories(){
         return categoryRepository.findAll();
+    }
+    @GetMapping("category/{id}")
+    ResponseEntity<?> getCategory(@PathVariable Long id){
+       Optional<Category> category = categoryRepository.findById(id);
+        return category.map(response -> ResponseEntity.ok().body(response))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 }
