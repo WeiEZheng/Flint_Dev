7b27295984d5376c9f1061f1cdc334a446d325cd
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-30 15:59:36 -0400

    User

--- src/main/java/com/example/flint/model/User.java
+++ src/main/java/com/example/flint/model/User.java
@@ -1,4 +1,14 @@
 package com.example.flint.model;
 
+import lombok.*;
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name="user")
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
 }
