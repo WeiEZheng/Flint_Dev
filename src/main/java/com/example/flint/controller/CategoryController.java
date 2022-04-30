7b028d45c84e7b76f49a075370eaad4962c7095f
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-30 16:57:10 -0400

    Category Controller

--- src/main/java/com/example/flint/controller/CategoryController.java
+++ src/main/java/com/example/flint/controller/CategoryController.java
@@ -1,8 +1,15 @@
 package com.example.flint.controller;
 
+import com.example.flint.repository.CategoryRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
+    private CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository){
+        super();
+        this.categoryRepository = categoryRepository;
+    }
 }
