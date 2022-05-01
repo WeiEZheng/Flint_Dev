4990cacde4dfb6a29029dcf6b8c1c598903fa2b9
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-05-01 01:55:29 -0400

    some routing

--- flint/src/App.js
+++ flint/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
+import BankAccounts from './components/BankAccounts';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
     <NavigationBar/>
+    <Router>
+    <Routes>
+    <Route path='/' exact={true} component={Home}/>
+    <Route path='/bankaccount' exact={true} component={BankAccounts}/>
+    </Routes>
+    </Router>
     </div>
   );
 }
---
+++ flint/src/components/BankAccounts.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class BankAccounts extends React.Component {
+
+constructor(props) {
+super(props);
+this.state = {bankAccounts: [], isLoading: true};
+this.remove = this.remove.bind(this);
+}
+
+   componentDidMount() {
+    this.setState({isLoading: true});
+
+    fetch('/bankaccount')
+    .then(response => response.json())
+    .then(data => this.setState({bankAccounts: data, isLoading: false}));
+  }
+
+  render() {
+    const {bankAccounts, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div>
+
+            <h2>Bank Accounts</h2>
+            {bankAccounts.map(bankAccount =>
+              <div key={bankAccount.id}>
+                {bankAccount.name}
+              </div>
+            )}
+
+      </div>
+    );
+  }
+}
+
+export default BankAccounts;
