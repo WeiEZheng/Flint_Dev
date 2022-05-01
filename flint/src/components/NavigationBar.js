678fa53809038ebe7217ec3ba25426dde8bd73b7
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-05-01 00:30:25 -0400

    logo

--- flint/src/components/NavigationBar.js
+++ flint/src/components/NavigationBar.js
@@ -6,8 +6,8 @@ class NavigationBar extends React.Component {
 render(){
 return (<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
+                  <img src="https://i.imgur.com/oZUMusi.png" alt="Fire Logo"/>
                   <a class="navbar-brand" href="#">Flint</a>
-
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
