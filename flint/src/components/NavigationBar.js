4487cd5c80fff4a61978337c0362c50ab00d9f7e
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-05-01 09:29:32 -0400

    Logo fix

--- flint/src/components/NavigationBar.js
+++ flint/src/components/NavigationBar.js
@@ -1,13 +1,12 @@
 import React from 'react';
-
-
+import brandLogo from '../../../src/static/images/brandLogo.png';
 class NavigationBar extends React.Component {
 
 
 render(){
 return (<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                  <img src="https://i.imgur.com/oZUMusi.png" alt="Fire Logo"/>
+                  <img src={brandLogo} alt="Fire Logo" />
                   <a class="navbar-brand" href="#">Flint</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
