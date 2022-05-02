1bdb59e8925b8b44929f983ba43389bbff005f61
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-05-01 20:00:33 -0400

    changed render structure

--- flint/src/components/BankAccounts.js
+++ flint/src/components/BankAccounts.js
@@ -23,18 +23,36 @@ class BankAccounts extends React.Component {
     }
 
     return (
-      <div>
-            <h2>Bank Accounts</h2>
-            {bankAccounts.map(bankAccount =>
-              <div key={bankAccount.id}>
-            <ul>
-
-            <li>    {bankAccount.accountName}</li>
-            </ul>
+       <>
+            <div className="container-fluid text-center">
+                <div className="row content">
+                  <div className="col-sm-2 sidenav">
+                    <p><a href="#">Link</a></p>
+                    <p><a href="#">Link</a></p>
+                    <p><a href="#">Link</a></p>
+                  </div>
+                  <div className="col-sm-8 text-left">
+                    <h1>Welcome to Flint Banking</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <hr></hr>
+                    <h3>Test</h3>
+                    <p>Lorem ipsum...</p>
+                  </div>
+                  <div className="col-sm-2 sidenav">
+                    <div className="well">
+                      <p>ADS</p>
+                    </div>
+                    <div className="well">
+                      <p>ADS</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-            )}
 
-      </div>
+              <footer className="container-fluid text-center">
+                <p>Copyright 2022 Flint Banking</p>
+              </footer>
+      </>
     );
   };
 }
