96a0a135442990811e93e44ae3e1f7b999227350
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-05-01 20:40:52 -0400

    charts component

--- flint/src/components/BankAccounts.js
+++ flint/src/components/BankAccounts.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import Chart from './Chart';
 
 class BankAccounts extends React.Component {
 
@@ -33,7 +34,7 @@ class BankAccounts extends React.Component {
                   </div>
                   <div className="col-sm-8 text-left">
                     <h1>Welcome to Flint Banking</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <Chart/>
                     <hr></hr>
                     <h3>Test</h3>
                     <p>Lorem ipsum...</p>
