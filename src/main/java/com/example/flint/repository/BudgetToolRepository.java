ef21bb2bd54de394f1eb001b91d134cf8fd9053d
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-05-03 22:13:20 -0400

    Setters for budget

--- src/main/java/com/example/flint/repository/BudgetToolRepository.java
+++ src/main/java/com/example/flint/repository/BudgetToolRepository.java
@@ -3,5 +3,9 @@ package com.example.flint.repository;
 import com.example.flint.model.BudgetTool;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+
 public interface BudgetToolRepository extends JpaRepository<BudgetTool, Long> {
+    BudgetTool findByName(String name);
+    BudgetTool findByAmountSpent (BigDecimal amountSpent);
 }
