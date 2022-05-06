9f6bde2a6c6637a424005e107e66a0d6c5a19f97
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-05-06 12:20:02 -0400

    Footer

--- flint/src/components/Footer.js
+++ flint/src/components/Footer.js
@@ -4,10 +4,9 @@ import'./Footer.css'
 
 const Footer = () => {
   return (
-    <div className={"footer"}>
-      <footer className="text-center" style={{background:'rgb(0, 0, 0, .85)'}}>
-        <span style={{color:'white',fontWeight:'bold', fontSize:'18px'}}>Copyright 2022 Flint Banking</span>
-
+    <div >
+      <footer className={"footer"}>
+       Copyright 2022 Flint Banking
       </footer>
     </div>
   );
