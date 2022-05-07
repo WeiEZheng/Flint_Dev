c49410e039d92317c5c1bd12e90017fd9e91fd97
Mike <29170001+mike-prentice@users.noreply.github.com>
2022-05-07 18:56:13 -0400

    balance charting

--- flint/src/components/AccountComp/Chart.js
+++ flint/src/components/AccountComp/Chart.js
@@ -1,53 +1,29 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import axios from 'axios';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
-class Chart extends React.Component {
-  render() {
+
+const Chart = () => {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    getAllBalances();
+  }, []);
+  
+  const getAllBalances = () => {
+    axios.get('api/balances/1')
+      .then((response) => {
+        const allData = response.data.allData;
+        setData(allData);
+        console.log(data);
+      })
+      .catch((err) => console.error(`Error: ${err}`));
+  }
+  
+
+
+  
     return (
       <LineChart
         width={500}
@@ -69,5 +45,5 @@ class Chart extends React.Component {
       </LineChart>
     );
   }
-}
+
 export default Chart;
