0da4c5632b37ea567781e6f8082b65feb44fb0fe
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-05-07 15:53:33 -0400

    Landing page

--- flint/src/components/Welcome.jsx
+++ flint/src/components/Welcome.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import homeLogo from './images/test.png';
 import {Link} from 'react-router-dom';
+import './Welcome.css';
 
 class Welcome extends Component {
   render() {
@@ -10,7 +11,9 @@ class Welcome extends Component {
         <div style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh'}}>
           <div className={'container'}>
             <Link to={""} ><img className={"container-fluid text-center"} src={homeLogo} alt={"The Flint logo: A flame"}/></Link>
+            <span className={'welcomeText'}> WELCOME to FLINT </span>
           </div>
+
         </div>
       </div>
 
