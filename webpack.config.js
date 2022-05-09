f621f002af705e130d80bf41a0feefe7b57f87ce
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-05-09 00:12:56 -0400

    working on recharts, semantic-ui-react errors with npm install

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,32 @@ module.exports = {
         filename: 'react-app.js'
     },
     module: {
-        rules: [{
+        rules: [
+        {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              },{
             test: /\.(js|jsx)$/,
             exclude: /node_modules/,
             loader: "babel-loader",
             options: {
                 presets: ['@babel/preset-env', '@babel/preset-react']
             }
-        }]
+        },
+               {
+                 test: /\.(png|svg|jpe?g|gif)$/,
+                 include: /images/,
+                 use: [
+                   {
+                     loader: 'file-loader',
+                     options: {
+                       name: '[name].[ext]',
+                       outputPath: 'images/',
+                       publicPath: 'images/'
+                     }
+                   }
+                 ]
+               },]
     },
     resolve: {
         extensions: ['.js', '.jsx']
